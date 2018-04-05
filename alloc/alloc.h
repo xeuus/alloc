b0a91c7f2053fd363744d33b9f1f01736d4562f2
Aryan <alikhaniaryan@live.com>
2018-04-06 02:49:47 +0430

    removed c allocator

---
+++ alloc/alloc.h
@@ -0,0 +1,13 @@
+#ifndef ALLOC_H
+#define ALLOC_H
+
+#include "Allocator.h"
+#include "CAllocator.h"
+#include "PoolAllocator.h"
+#include "FreeListAllocator.h"
+#include "StackAllocator.h"
+#include "LinearAllocator.h"
+
+
+#endif // ALLOC_H
+
